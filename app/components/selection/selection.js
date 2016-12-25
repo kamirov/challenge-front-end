689df8b4de4709869aeb4207e5e6405a2989515f
Andrei Khramtsov <andrei.khramtsov@gmail.com>
2016-12-25 18:35:40 -0500

    Added static selection steps and progress bar

--- app/components/selection/selection.js
+++ app/components/selection/selection.js
@@ -1,8 +1,36 @@
 (function() {
    angular.module('FrontEndChallenge')
-      .controller('selectionCtrl', selectionCtrl);
+      .controller('selectionCtrl', selectionCtrl)
+      .directive('step', stepDirective);
+
 
    function selectionCtrl() {
+      var vm = this;
+
+      // Steps in the process
+      vm.steps = {
+         items: ['Profile', 'Offers', 'Success'],
+         currentItem: 1
+      };
+   }
+
+
+   function stepDirective() {
+      return {
+         restrict: 'E',
+         templateUrl: 'app/components/selection/step.html',
+         scope: {
+            status: '@',
+            idx: '@',
+            name: '@'
+         },
+         controller: stepCtrl,
+         controllerAs: 'step'
+      };
+   }
+
+
+   function stepCtrl() {
       
    }
 
