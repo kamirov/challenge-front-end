61da51ad1c87867eb14ba3686328905b2c8c1173
Andrei Khramtsov <andrei.khramtsov@gmail.com>
2016-12-25 16:51:13 -0500

    Added JSON response,  root template, angular entry

--- app/app.module.js
+++ app/app.module.js
@@ -1,1 +1,11 @@
-angular.module('app', []);
+// Angular entry point. Defines root template directive.
+
+(function() {
+   angular.module('FrontEndChallenge', [])
+          .directive('app', function() {
+             return {
+                restrict: 'E',
+                templateUrl: 'app/app.html'
+             };
+          });
+})();
