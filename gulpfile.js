b4ec10cf273dffac5873c172d69db27a2a547a05
Andrei Khramtsov <andrei.khramtsov@gmail.com>
2016-12-25 20:03:06 -0500

    Progress steps styles (mostly) completed

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
-// Sass configuration
+// Gulp modules
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefix = require('gulp-autoprefixer');
+var minifyCSS = require('gulp-minify-css');
 
 var stylesDir = 'assets/css/';
 
@@ -8,6 +10,8 @@ gulp.task('styles', function() {
     gulp.src(stylesDir + '*.scss')
         .pipe(sass())
         .on('error', (err) => console.log(err) )
+        .pipe(autoprefix('last 2 versions'))
+        .pipe(minifyCSS())
         .pipe(gulp.dest(function(f) {
             return f.base;
         }))
