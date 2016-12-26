c56488857d47b33323d338b38c63d69e393ce7b4
Andrei Khramtsov <andrei.khramtsov@gmail.com>
2016-12-26 13:05:40 -0500

    Selection page styles and functionality done (no persistence)

--- app/components/selection/selection.js
+++ app/components/selection/selection.js
@@ -18,6 +18,10 @@
       var vm = this;
 
       // Get selected offers
+      vm.selected = [];
+
+      vm.maxSelections = 5;
+
       // TBA
 
       // Get data
@@ -32,6 +36,33 @@
          items: ['Profile', 'Offers', 'Success'],
          currentItem: 1
       };
+
+      this.isSelected = function(offerId) {
+         return vm.selected.includes(offerId);
+     }
+
+      this.handleSelect = function(offerId) {
+
+         // Find the offer 
+         var offer = vm.offers.find(function(item) {
+            return item.id === offerId;
+         });
+
+         var offerIdx = vm.selected.indexOf(offer.id);
+
+         // Toggle selection
+         if (offerIdx === -1) {
+            if (!vm.reachedMaxSelections())
+               vm.selected.push(offer.id);
+         }
+         else
+            vm.selected.splice(offerIdx, 1);
+      }
+
+      this.reachedMaxSelections = function() {
+         // console.log(vm.selected, vm.selected.length, vm.maxSelections);
+         return vm.selected.length === vm.maxSelections;
+      }
    }
 
    function offerDirective() {
